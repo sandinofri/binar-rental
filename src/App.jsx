fbca50222fead27a657a6dab3291e08b016786ab
Richie-Z <zakariarichie@yahoo.com>
2023-12-15 19:22:16 +0700

    feat(admin): introduce admin routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery';
 import 'popper.js';
 import 'bootstrap/dist/js/bootstrap';
-import { routes } from '@/modules/Landing/routes';
+import { routes as LandingRoutes } from '@/modules/Landing/routes';
+import { routes as AdminRoutes } from '@/modules/Admin/routes';
 import { useRoutes } from 'react-router-dom';
 
 function App() {
-  let route = useRoutes(routes)
+  let route = useRoutes([...LandingRoutes, ...AdminRoutes])
   return route
 }
 
