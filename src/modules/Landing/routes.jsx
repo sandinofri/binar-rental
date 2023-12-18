beb8d499f0243844ae331c6fe610169ef44cdd37
Elgara5364 <steveloardi5364@gmail.com>
2023-12-18 21:26:04 +0900

    add sign in and sign up in components

--- src/modules/Landing/routes.jsx
+++ src/modules/Landing/routes.jsx
@@ -1,31 +1,28 @@
 import DetailCarPage from "./pages/DetailCarPage";
 import LandingPage from "./pages/LandingPage";
 import NotFound from "./pages/NotFound";
+import Register from "./pages/Register";
 import SearchResult from "./pages/SearchResult";
 
 export const routes = [
   {
     path: "/",
-    element: (
-      <LandingPage />
-    ),
+    element: <LandingPage />,
   },
   {
     path: "/search",
-    element: (
-      <SearchResult />
-    ),
+    element: <SearchResult />,
   },
   {
     path: "/detail/:id",
-    element: (
-      <DetailCarPage />
-    ),
+    element: <DetailCarPage />,
   },
   {
     path: "*",
-    element: (
-      <NotFound />
-    ),
+    element: <NotFound />,
   },
-]
+  {
+    path: "/register",
+    element: <Register />,
+  },
+];
