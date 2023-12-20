21f23557dca6958af1675c12c6aeb5aa664ea3b8
Elgara5364 <steveloardi5364@gmail.com>
2023-12-21 00:06:13 +0900

    fix margin on footer component

---
+++ src/modules/Landing/components/Calendars/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useEffect, useRef, useState } from "react";
+
+const Calendars = () => {
+  return (
+    <div>
+      <h1>Calendar Components</h1>
+    </div>
+  );
+};
+
+export default Calendars;
