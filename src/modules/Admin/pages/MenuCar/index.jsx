fc34bbdc42473f565d5af07331fefbfbed72f8d6
tsugabsa20 <bagusbintoro96@gmail.com>
2023-12-24 21:00:28 +0700

    editing stylish

--- src/modules/Admin/pages/MenuCar/index.jsx
+++ src/modules/Admin/pages/MenuCar/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "./style.css"
 import { getMenu } from "../../redux/features/menuCar/menuSlicer";
 import { useDispatch, useSelector } from "react-redux";
+import Navbar from "../../component/Navbar";
 const MenuCar = () => {
   const dispatch = useDispatch()
   const {list}= useSelector((state) => state.menuCar)
@@ -11,27 +12,28 @@ useEffect(() => {
 },[])
   
   return (
-    <div>
-      <h1>Menu</h1>
-      <div className="container">
+    <div className="container-bg">
+      <Navbar/>
+      <div className="information-cars">
+        <p>List Cars</p>
+        <button>Add New Car</button>
+      </div>
+      <div className="container-post-card">
       {list.map((item, index) => (
-      
         <div key={index}>
-          <div className="text">
-            <img className="image" src={item.image}/>
-            <p>{item.name}</p>
-            <p>{item.category}</p>
-            <p>Rp {item.price}</p>
-          </div>
-          <div className="button">
-            <button className="delete">Delete</button>
-            <button className="edit">Edit</button>
-            
+          <div className="post-card">
+            <div className="text">
+              <img className="image" src={item.image}/>
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>Rp {item.price}</p>
+            </div>
+            <div className="button">
+              <button className="delete">Delete</button>
+              <button className="edit">Edit</button>
+            </div>
           </div>
         </div>
-          
-     
-      
      ))}
       
         </div>
