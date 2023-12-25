d42b83790448c5a8c8175e77e1192013f28720ab
Elgara5364 <steveloardi5364@gmail.com>
2023-12-26 02:09:07 +0900

    Add Empty Hero Components

---
+++ src/modules/Landing/components/EmptyHero/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+const EmptyHero = () => {
+  return <div className="hero"></div>;
+};
+
+export default EmptyHero;
