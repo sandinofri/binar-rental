4eed11e603e363a78d8febfe744d85c6332f3235
Richie-Z <zakariarichie@yahoo.com>
2023-12-28 20:45:23 +0700

    refactor(admin): components and layout directory

---
+++ src/modules/Admin/components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+import SmallLogo from "@/modules/Admin/assets/images/small-brand.svg?react"
+import LargeLogo from "@/modules/Admin/assets/images/large-brand.svg?react"
+
+export default function Navbar() {
+  return (
+    <nav className="main-navbar">
+      <SmallLogo />
+    </nav>
+  )
+}
---
+++ src/modules/Admin/layouts/MainLayout/index.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../../components/Sidebar"
+import Navbar from '../../components/Navbar'
+
+export default function MainLayout() {
+  return (
+    <div id="app">
+      <Navbar />
+      <Sidebar />
+    </div>
+  )
+
+}
