23c32247704179d2995c436786c4c6c7ddd90373
Richie-Z <zakariarichie@yahoo.com>
2023-12-28 19:15:19 +0700

    refactor(admin): login admin style

--- src/modules/Admin/pages/SignIn/index.jsx
+++ src/modules/Admin/pages/SignIn/index.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import './login.css'
-import loginimage from "../../assets/image/login-image.png"
 
 const SignIn = () => {
   return (
@@ -8,24 +6,21 @@ const SignIn = () => {
       <section>
         <div className="row min-vh-100">
           <div className="col-lg-8 left">
-            <div className="gambar">
-              <img src={loginimage} alt="" />
-            </div>
           </div>
           <div className="col-lg-4 d-flex justify-content-center ">
             <div className="right">
               <div className="logo"></div>
-              <h1>welcome, admin BCR</h1>
+              <h1>Welcome, Admin BCR</h1>
               <form action="">
                 <div className="d-flex flex-column mb-3">
-                  <label htmlFor="" className="mb-2">email</label>
-                  <input type="text" placeholder="ex:admin@mail.com" />
+                  <label htmlFor="" className="mb-2">Email</label>
+                  <input type="text" placeholder="Contoh: johndee@gmail.com" />
                 </div>
                 <div className="d-flex flex-column">
-                  <label htmlFor="" className='mb-2'>password</label>
+                  <label htmlFor="" className='mb-2'>Password</label>
                   <input type="password" placeholder="6+ caracter" />
                 </div>
-                <a className="text-center">sign In</a>
+                <a className="text-center">Sign In</a>
               </form>
             </div>
           </div>
