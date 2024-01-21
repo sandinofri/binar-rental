66d87b3ed8821f3a2b6e9005e056615a08c7bdb7
Richie-Z <zakariarichie@yahoo.com>
2024-01-21 22:53:52 +0700

    feat(admin): admin popover logout

--- src/modules/Admin/layouts/MainLayout/index.jsx
+++ src/modules/Admin/layouts/MainLayout/index.jsx
@@ -2,8 +2,17 @@ import Sidebar from "../../components/Sidebar"
 import Navbar from '../../components/Navbar'
 import './style.scss'
 import { SidebarDetail } from "../../components/SidebarDetail"
+import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
 
 export default function MainLayout({ menu, children, menuTitle }) {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (localStorage.getItem('token') === null && localStorage.getItem('accessToken') === null) {
+      navigate('/admin/login')
+    }
+  }, [navigate])
   return (
     <div id="app">
       <Navbar />
