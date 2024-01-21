3a543aeae3cec251318ac030f76777a0b8a33100
Richie-Z <zakariarichie@yahoo.com>
2024-01-21 23:15:43 +0700

    feat(components): introduce breadcrumb admin

--- src/modules/Admin/pages/Cars/Cars.jsx
+++ src/modules/Admin/pages/Cars/Cars.jsx
@@ -4,17 +4,12 @@ import MainLayout from "../../layouts/MainLayout";
 import Vector from "../../../admin/assets/icons/Vector.png";
 import { Link } from "react-router-dom";
 import MENU_LISTS from "../../constants/menuLists";
+import { Breadcrumb } from "../../components/Breadcrumb";
 
 const Cars = () => {
   return (
     <MainLayout menu={MENU_LISTS[1]} menuTitle="List Car">
-      <div className="d-flex gap-2">
-        <p className="car">Cars</p>
-        <div>
-          <img src={Vector} alt="vector" />
-        </div>
-        <p className="List-car">List Car</p>
-      </div>
+      <Breadcrumb currentLink="List Car" previousLink="Cars" />
 
       <div className="d-flex justify-content-between align-items-center pe-3 mt-4">
         <p className="list-car2">List Car</p>
