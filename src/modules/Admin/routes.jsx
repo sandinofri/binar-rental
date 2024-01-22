3ad0fef5a60417848594e7b2331fdfdd6030b678
Richie-Z <zakariarichie@yahoo.com>
2024-01-22 21:46:11 +0700

    fix(admin): duplicate component; move menu cars to list cars

--- src/modules/Admin/routes.jsx
+++ src/modules/Admin/routes.jsx
@@ -2,7 +2,6 @@ import AddCar from "./pages/Cars/AddCar";
 import Cars from "./pages/Cars/Cars";
 import Dashboard from "./pages/Dasboard";
 import SignIn from "./pages/SignIn";
-import MenuCar from "./pages/MenuCar";
 export const routes = [
   {
     path: "/admin",
@@ -18,8 +17,4 @@ export const routes = [
   },
   { path: "/admin/cars/add", element: <AddCar /> },
 
-  {
-    path: "/admin/menu",
-    element: <MenuCar />,
-  },
 ];
