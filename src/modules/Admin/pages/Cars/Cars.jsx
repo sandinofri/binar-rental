2c1e9b2c1c18fa9525b6c247211afda7347e84eb
Richie-Z <zakariarichie@yahoo.com>
2024-01-26 22:48:38 +0700

    feat(admin): car admin cattegory dynamic

--- src/modules/Admin/pages/Cars/Cars.jsx
+++ src/modules/Admin/pages/Cars/Cars.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./style.scss";
 import MainLayout from "../../layouts/MainLayout";
 import { Link } from "react-router-dom";
@@ -11,6 +11,14 @@ import { Car } from "../../components/Car";
 const Cars = () => {
   const dispatch = useDispatch()
   const { list } = useSelector((state) => state.menuCar)
+  const categories = [
+    { name: 'All', value: undefined },
+    { name: '2 - 4 People', value: 'small' },
+    { name: '4 - 6 People', value: 'medium' },
+    { name: '6 - 8 People', value: 'large' },
+  ]
+
+  const [activeCategory, setActiveCategory] = useState(undefined)
 
   useEffect(() => {
     dispatch(getMenu())
@@ -19,16 +27,15 @@ const Cars = () => {
     <MainLayout menu={MENU_LISTS[1]} menuTitle="List Car">
       <Breadcrumb currentLink="List Car" previousLink="Cars" />
 
-      <div className="d-flex justify-content-between align-items-center pe-3 mt-4">
+      <div className="d-flex justify-content-between align-items-center pe-4 mt-4">
         <p className="list-car2">List Car</p>
         <Link className="add-car" to={'/admin/cars/add'}><span>+</span>Add New Car</Link>
       </div>
 
       <div className="category-car">
-        <button>All</button>
-        <button>2 - 4 people</button>
-        <button>4 - 6 people</button>
-        <button>6 - 8 people</button>
+        {categories.map((item, index) => (
+          <button key={index} className={activeCategory === item.value ? 'active' : ''} onClick={() => setActiveCategory(item.value)}>{item.name}</button>
+        ))}
       </div>
       <div className="container-post-card">
         {list.map((item, index) => (
