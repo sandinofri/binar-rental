cb54b2e3f1c5f4c018385d20f60f2f00dc54562e
Richie-Z <zakariarichie@yahoo.com>
2024-01-27 18:54:17 +0700

    chore(admin): remove unused style

--- src/contex/NotificationContex.jsx
+++ src/contex/NotificationContex.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import { createContext,useState } from 'react'
+import { createContext, useState } from 'react'
 
 export const NotificationContex = createContext()
 
-const NotificationProvider = ({children}) => {
-
-    const[notification,setNotification]=useState("")
-    const setNotif =(notif)=>{
-        setNotification(notif)
-       }
+const NotificationProvider = ({ children }) => {
+  const [notification, setNotification] = useState("")
+  const setNotif = (notif) => {
+    setNotification(notif)
+  }
   return (
-    <NotificationContex.Provider value={{setNotif,notification}}>{children}</NotificationContex.Provider>
+    <NotificationContex.Provider value={{ setNotif, notification }}>{children}</NotificationContex.Provider>
   )
 }
 
