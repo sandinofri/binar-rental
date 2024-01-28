f6f3c787b73aff79f2863f5085727555125f14e5
Richie-Z <zakariarichie@yahoo.com>
2024-01-28 23:19:43 +0700

    feat(admin): add pagination ability in dashboard

--- src/modules/Admin/pages/Dasboard/tableData/table.jsx
+++ src/modules/Admin/pages/Dasboard/tableData/table.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
+import PrevIcon from "@/modules/Admin/assets/icons/fi_chevrons-left.svg?react"
+import NextIcon from "@/modules/Admin/assets/icons/fi_chevrons-right.svg?react"
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import moment from 'moment/moment';
@@ -61,6 +63,26 @@ const TableData = () => {
         }
     }
 
+    const paginationNumberButton = () => {
+        return Array.from({ length: 3 }, (_, index) => {
+            if (options.currentPage + index >= Math.ceil(totalRecords / options.pageSize)) return
+            return <button
+                className='number'
+                key={options.currentPage + index}
+                onClick={() => handlePagination(options.currentPage + index)}
+                disabled={options.currentPage === options.currentPage + index}
+            >{options.currentPage + index}</button>
+        });
+    }
+
+    const handlePagination = async (page) => {
+        setOptions({
+            ...options,
+            currentPage: page
+        })
+        await getTable()
+    }
+
     return (
         <>
             <DataTable value={data}>
@@ -69,19 +91,42 @@ const TableData = () => {
                 ))}
             </DataTable >
             <div className="table-footer">
-                <div className="input">
-                    <p>Limit</p>
-                    <select defaultValue={options.pageSize} onChange={(e) => setOptions({ ...options, pageSize: Number(e.target.value) })}  >
-                        <option value="10">10</option>
-                        <option value="50">50</option>
-                    </select>
+                <div className="input-groups">
+                    <div className="input">
+                        <p>Limit</p>
+                        <select defaultValue={options.pageSize} onChange={(e) => setOptions({ ...options, pageSize: Number(e.target.value) })}  >
+                            <option value="10">10</option>
+                            <option value="50">50</option>
+                        </select>
+                    </div>
+                    <div className="input">
+                        <p>Jump to page</p>
+                        <select name="" onChange={(e) => setOptions({ ...options, currentPage: Number(e.target.value) })} defaultValue={options.currentPage} value={options.currentPage}>
+                            {Array.from({ length: Math.ceil(totalRecords / options.pageSize) }, (_, i) => i + 1).map((page) => <option key={page} value={page}>{page}</option>)}
+                        </select>
+                        <button onClick={() => getTable()}>Go</button>
+                    </div>
                 </div>
-                <div className="input">
-                    <p>Jump to page</p>
-                    <select name="" onChange={(e) => setOptions({ ...options, currentPage: Number(e.target.value) })} defaultValue={options.currentPage} >
-                        {Array.from({ length: Math.ceil(totalRecords / options.pageSize) }, (_, i) => i + 1).map((page) => <option key={page} value={page}>{page}</option>)}
-                    </select>
-                    <button onClick={() => getTable()}>Go</button>
+                <div className="table-pagination">
+                    <button
+                        disabled={options.currentPage === 1}
+                        onClick={() => handlePagination(options.currentPage - 1)}
+                    >
+                        <PrevIcon />
+                    </button>
+                    {paginationNumberButton()}
+                    <button>...</button>
+                    <button
+                        className='number'
+                        disabled={options.currentPage === Math.ceil(totalRecords / options.pageSize)}
+                        onClick={() => handlePagination(Math.ceil(totalRecords / options.pageSize))}>{Math.ceil(totalRecords / options.pageSize)}</button>
+                    <button
+                        disabled={options.currentPage === Math.ceil(totalRecords / options.pageSize)}
+                        onClick={() => handlePagination(options.currentPage + 1)}
+                    >
+                        <NextIcon />
+                    </button>
+
                 </div>
             </div>
         </>
