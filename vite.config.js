2ff19c19b69aca4a5ce4cf91dab0dbffecfa19e6
sandinofri <sandinofri92@gmail.com>
2024-03-11 22:11:07 +0700

    add gh pages configuration

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/binar-rental/",
   plugins: [react(), svgr()],
   resolve: {
     alias: {
